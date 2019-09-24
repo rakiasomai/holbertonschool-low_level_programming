67e42e2e33f5efd21f13e018f9b16ac9febccf70
somairakia <somai.rakia@hotmail.fr>
2019-09-24 14:58:58 +0000

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 - prints the alphabet from a to z
+ *
+ *Return: 0;
+ */
+
+void print_alphabet_x10(void)
+{
+int letter, i;
+letter = 'a';
+for (i = 0; i < 10; i++)
+{
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+letter = 'a';
+}
+}
