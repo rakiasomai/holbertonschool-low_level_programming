ad5b7611e4bb38a179e7cd74d63b8a1b3a16231b
somairakia <somai.rakia@hotmail.fr>
2019-09-24 14:19:37 +0000

    commit

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* main - Entry
+*
+* Return: Always 0
+*/
+int main(void)
+{
+char ch[] = "Holberton\n";
+int i;
+for (i = 0; i <= 9; i++)
+{
+_putchar(ch[i]);
+}
+return (0);
+}
