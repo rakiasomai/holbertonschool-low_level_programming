5fcf4345e9fce163cead2e47792ba1941138c948
somairakia <somai.rakia@hotmail.fr>
2019-09-26 12:57:47 +0000

    commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,9 +2,10 @@
 #include "holberton.h"
 
 /**
-* int mul(int a, int b) -  function that multiplies two integers.
-* @a @b
-* return : always 0.
+* _mul - (int a, int b) function that multiplies two integers.
+* @a: is an integer
+* @b: is an integer
+* Return: always 0.
 */
 
 int mul(int a, int b)
