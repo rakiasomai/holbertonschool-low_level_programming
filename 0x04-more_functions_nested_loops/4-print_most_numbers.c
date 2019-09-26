619f6095d75d7c04ec7e379b0066131ae265d913
somairakia <somai.rakia@hotmail.fr>
2019-09-26 13:19:45 +0000

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
 #include "holberton.h"
 
 /**
-* print_most_numbers - from 0 to 9 without 2 and 4.
-* Return: Always 0.
+* print_most_numbers - printing 0 -9 exluding 2 and 4
+*
+* Return: none;
 */
+
 void print_most_numbers(void)
 {
 int c;
-for (c = 0 ; c <= 9; c++)
+
+for (c = 0; c <= 9; c++)
 {
 if (c != 2 && c != 4)
 _putchar(c + '0');
