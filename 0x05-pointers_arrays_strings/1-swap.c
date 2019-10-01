d5717ef698873b8f80c6e61a4e0ff9334b2b748b
somairakia <somai.rakia@hotmail.fr>
2019-10-01 10:34:16 +0000

    commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* swap_int - swaps the values of two integers.
+* @a : is an integer
+* @b : is an integer
+* Return: 0
+*/
+void swap_int(int *a, int *b)
+{
+int y;
+y = a;
+a = b;
+b = y;
+}
