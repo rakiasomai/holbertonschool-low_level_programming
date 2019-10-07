9d759b6b291f69fe3c2d622fe386c9dfdb30836a
somairakia <somai.rakia@hotmail.fr>
2019-10-07 13:25:25 +0000

    commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* *_strch - function that locates a character in a string
+* @s: is a string
+* @c : is the charcater to locate
+* Return: c
+*/
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+return (s);
+else if (*(s + 1) == c)
+return (s + 1);
+s++;
+}
+return (s + 1);
+}
