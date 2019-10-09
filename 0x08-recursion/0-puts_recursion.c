79ca2daff860e619141a6da04527f734a2b5c915
somairakia <somai.rakia@hotmail.fr>
2019-10-09 13:37:56 +0000

    commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* _puts_recursion - function that prints a string
+* @s: is a string
+*
+* Return: 0
+*/
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+_putchar('\n');
+else
+{
+_putchar(*s);
+_puts_recursion(++s);
+}
+}
