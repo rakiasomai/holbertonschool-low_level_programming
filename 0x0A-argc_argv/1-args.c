0096d8bcd282a01fe5abe5ad45b59e59cb98bd55
somairakia <somai.rakia@hotmail.fr>
2019-10-13 11:08:50 +0000

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* main - program that prints the number of arguments passed into it.
+* @argc: is an argument.
+* @argv: is an arguments
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc - 1);
+return (0);
+}
