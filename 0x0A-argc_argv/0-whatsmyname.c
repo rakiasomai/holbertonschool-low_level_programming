9a1d10e1fcc643328be10700fbe179763e077031
somairakia <somai.rakia@hotmail.fr>
2019-10-13 11:05:35 +0000

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* main - program that prints its name.
+* @argc: is an argument
+* @argv: is an agruments.
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
