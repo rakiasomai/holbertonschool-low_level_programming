7cb3ba85aa554025195db9e01798cdbefd04c0fa
somairakia <somai.rakia@hotmail.fr>
2019-10-14 11:56:56 +0000

    commit

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,1 +1,12 @@
-int _atoi(char *s);
+#include "holberton.h"
+/**
+* _atoi - converts a string to an integer
+* @s: string to convert
+*
+* Return: value of integer
+*/
+int _atoi(char *s)
+{
+return (0);
+}
+
