f7382d84430551d4302772fbfbed26ab65ab0a3e
somairakia <somai.rakia@hotmail.fr>
2019-10-22 09:20:51 +0000

    commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,7 +10,7 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new_dog = new_dog = malloc(sizeof(dog_t));
+dog_t *new_dog =  malloc(sizeof(dog_t));
 int i, j, y;
 
 if (new_dog == NULL)
@@ -18,19 +18,14 @@ return (NULL);
 
 for (i = 0; name[i]; i++)
 ;
+for (j = 0; owner[j]; j++)
+;
 new_dog->name = malloc((i + 1) * sizeof(char));
 if (new_dog == NULL)
 {
 free(new_dog);
 return (NULL);
 }
-for (y = 0; y <= i; y++)
-new_dog->name[y] = name[y];
-
-
-
-for (j = 0; owner[j]; j++)
-;
 new_dog->owner = malloc((j + 1) * sizeof(char));
 if (new_dog == NULL)
 {
@@ -38,6 +33,9 @@ free(new_dog->name);
 free(new_dog);
 return (NULL);
 }
+
+for (y = 0; y <= i; y++)
+new_dog->name[y] = name[y];
 new_dog->age = age;
 for (y = 0; y <= j; y++)
 new_dog->owner[y] = owner[y];
