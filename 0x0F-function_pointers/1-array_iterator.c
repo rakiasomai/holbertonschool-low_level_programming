8f359deb93c7f3ff315209ccbbc8f4786e4b1a46
somairakia <somai.rakia@hotmail.fr>
2019-10-23 15:27:29 +0000

    commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
-#include <stdlib.h>
 /**
 * array_iterator -  function that executes a function given as a parameter
 * @array: is an array of integers
@@ -11,7 +9,10 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t y;
+
 if (array != NULL && action != NULL)
+{
 for (y = 0; y < size; y++)
 action(array[y]);
+}
 }
