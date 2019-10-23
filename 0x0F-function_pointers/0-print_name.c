5ecfd203d4726e6131e14a324a37e0154209b998
somairakia <somai.rakia@hotmail.fr>
2019-10-23 13:35:04 +0000

    commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,9 +3,10 @@
 * print_name - function that prints a name
 * @name: is a char
 * @f: is a function to a pointer
+* Return: Void
 */
 void print_name(char *name, void (*f)(char *))
 {
-if (name =! NULL && f =! NULL)
+if (name != NULL && f != NULL)
 f(name);
 }
