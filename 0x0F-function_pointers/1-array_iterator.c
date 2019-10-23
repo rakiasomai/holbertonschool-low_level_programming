2dede24a9c1e8f6fee28644d238a468f883a618e
somairakia <somai.rakia@hotmail.fr>
2019-10-23 15:19:26 +0000

    commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
 * array_iterator -  function that executes a function given as a parameter
 * @array: is an array of integers
