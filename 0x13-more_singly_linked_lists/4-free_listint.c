a054cff35a5b7f4618d250f3110f25b0879126d9
somairakia <somai.rakia@hotmail.fr>
2019-11-05 16:29:34 +0000

    commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+* free_listsint - function that frees a list.
+* @head: is a pointer.
+* Return: Nothing.
+*/
+void free_listint(listint_t *head)
+{
+listint_t *p;
+while (head != NULL)
+{
+p = head;
+head->next = head;
+free(p);
+}
+}
