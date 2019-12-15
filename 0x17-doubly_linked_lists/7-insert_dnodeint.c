02d2665fba0e4003de2528f34fc870f56926d2cb
somairakia <somai.rakia@hotmail.fr>
2019-12-15 13:57:26 +0000

    fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,48 +1,50 @@
 #include "lists.h"
 /**
-* insert_dnodeint_at_idx - insert a new node at given position
-* @h: is a double pointer
-* @idx: is an integer
-* @n: is an integer
-* Return: Address of new node, or NULL if failed
-*/
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+ * insert_dnodeint_at_idx - function thatinsert a new node at given position
+ * @h: is a double pointer
+ * @idx: is an integer
+ * @n: is an inetger
+ * Return: Address of new node, or NULL if failed
+ */
+dlistint_t *insert_dnodeint_at_idx(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new, *temp = *h;
-if (h == NULL)
-return (NULL);
+unsigned int c;
+dlistint_t *tmp, *prev, *new;
 new = malloc(sizeof(dlistint_t));
 if (new == NULL)
 return (NULL);
 new->n = n;
-if (idx == 0 && *h == NULL)
+for (tmp = *h, c = 1; tmp && c < idx; c++, tmp = tmp->next)
+prev = tmp;
+if (idx == 0)
 {
-new->prev = NULL;
-new->next = NULL;
-*h = new;
+*h = new; new->prev = NULL;
+new->next = (tmp == NULL) ? NULL : tmp;
 return (new);
 }
-if (idx == 0 && temp != NULL)
+if (idx == 1)
 {
-new->next = temp, new->prev = NULL;
-temp->prev = new, *h = new;
+prev = *h;
+tmp = ((*h)->next == NULL) ? NULL : (*h)->next;
+new->prev = prev; new->next = tmp; prev->next = new;
+if (tmp)
+tmp->prev = new;
 return (new);
 }
-while (temp != NULL && idx > 1)
-temp = temp->next, idx--;
-if (temp != NULL)
+if (idx == c && tmp == NULL)
 {
-new->next = temp->next;
-new->prev = temp;
-if (temp->next == NULL)
-temp->next = new;
-else
+if (prev != NULL)
 {
-temp->next->prev = new;
-temp->next = new;
+new->prev = prev; new->next = NULL;
+prev->next = new; return (new);
 }
+free(new); return (NULL);
+}
+else if (idx != c && tmp == NULL)
+{
+free(new); return (NULL);
+}
+prev = tmp; tmp = tmp->next; new->prev = prev;
+new->next = tmp; prev->next = new; tmp->prev = new;
 return (new);
-}
-free(new);
-return (NULL);
 }
