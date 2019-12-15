fb671d67dd537244ce193ec11670a48726a1d57d
somairakia <somai.rakia@hotmail.fr>
2019-12-15 13:18:02 +0000

    fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
-* insert_dnodeint_at_idx - function that insert a new node at given position
+* insert_dnodeint_at_idx - insert a new node at given position
 * @h: is a double pointer
 * @idx: is an integer
 * @n: is an integer
@@ -9,29 +9,29 @@
 dlistint_t *insert_dnodeint_at_idx(dlistint_t **h, unsigned int idx, int n)
 {
 unsigned int c;
-dlistint_t *y, *prev, *new;
+dlistint_t *tmp, *prev, *new;
 new = malloc(sizeof(dlistint_t));
 if (new == NULL)
 return (NULL);
 new->n = n;
-for (y = *h, c = 1; y && c < idx; c++, y = y->next)
-prev = y;
+for (tmp = *h, c = 1; tmp && c < idx; c++, tmp = tmp->next)
+prev = tmp;
 if (idx == 0)
 {
 *h = new; new->prev = NULL;
-new->next = (y == NULL) ? NULL : y;
+new->next = (tmp == NULL) ? NULL : tmp;
 return (new);
 }
 if (idx == 1)
 {
 prev = *h;
-y = ((*h)->next == NULL) ? NULL : (*h)->next;
-new->prev = prev; new->next = y; prev->next = new;
-if (y)
-y->prev = new;
+tmp = ((*h)->next == NULL) ? NULL : (*h)->next;
+new->prev = prev; new->next = tmp; prev->next = new;
+if (tmp)
+tmp->prev = new;
 return (new);
 }
-if (idx == c && y == NULL)
+if (idx == c && tmp == NULL)
 {
 if (prev != NULL)
 {
@@ -40,11 +40,11 @@ prev->next = new; return (new);
 }
 free(new); return (NULL);
 }
-else if (idx != c && y == NULL)
+else if (idx != c && tmp == NULL)
 {
 free(new); return (NULL);
 }
-prev = y; y = y->next; new->prev = prev;
-new->next = y; prev->next = new; y->prev = new;
+prev = tmp; tmp = tmp->next; new->prev = prev;
+new->next = tmp; prev->next = new; tmp->prev = new;
 return (new);
 }
