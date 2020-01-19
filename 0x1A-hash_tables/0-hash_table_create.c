4bc926e4905eb6601a77e96533b22233e0cdff1d
somairakia <somai.rakia@hotmail.fr>
2020-01-19 16:08:01 +0000

    commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,3 +1,10 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that creates a hash table.
+ * @size : size of the table
+ * Return: pointer to the created table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *tab = NULL;
