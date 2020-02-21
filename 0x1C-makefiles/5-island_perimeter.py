5ab0938faef45202e23ce590f3811d928363a5bd
rakiasomai <1069@holbertonschool.com>
2020-02-21 21:04:35 +0000

    commit

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    """calculate the perimeter of an island """
+    perm = 0 
+    link = 0
+
+    for y in range(len(grid)):
+        for k in range(len(grid[y])):
+            if grid[y][k] == 1:
+                perm += 4
+
+                if y <> 0 and grid[y-1][k] == 1:
+                    link += 1
+                if k <> 0 and grid[y][k-1] == 1:
+                    link += 1
+
+    return(perm - (2 * link))
