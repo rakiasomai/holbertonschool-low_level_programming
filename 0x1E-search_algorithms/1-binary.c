a6433725925094e1d1616281a6381b9b86f2e6d4
rakiasomai <1069@holbertonschool.com>
2020-04-15 14:43:23 +0000

    update

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,57 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* binary_search - binary search a ascending sorted array
+* @array: this is a pointer
+* @size: number of elements in array
+* @value: value to search for
+* Return: -1 if error or not found
+*/
+int binary_search(int *array, size_t size, int value)
+{
+int take;
+size_t a = 0;
+if (array == NULL || size == 0)
+return (-1);
+size--;
+while (a <= size)
+{
+print_array(array, a, size);
+take = (size - a) / 2 + a;
+if (array[take] == value)
+return (take);
+else if (array[take] < value)
+{
+if (size % 2 == 1)
+a = take + 1;
+else
+a = take;
+}
+else
+{
+if (size % 2 == 1)
+size = take;
+else
+size = take - 1;
+}
+}
+return (-1);
+}
+/**
+* print_array - printing the current array
+* @array: this is an array
+* @add: starting incrementer index
+* @size: the size ofincremeneted idex
+*/
+void print_array(int *array, size_t add, size_t size)
+{
+printf("Searching in array: ");
+for (add = 0; add <= size; add++)
+{
+printf("%d", array[add]);
+if (add < size)
+printf(", ");
+}
+printf("\n");
+}
