d5dae33fb573e83a4d1bd278e7a6205179f5ce6a
rakiasomai <1069@holbertonschool.com>
2020-04-15 14:30:15 +0000

    fix

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,7 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+#include <stdio.h>
+void print_array(int *array, size_t inc, size_t size);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 #endif
