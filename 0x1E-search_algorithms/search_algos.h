5a26910463772a5dec19cfe8b543dd7698b7d7ca
rakiasomai <1069@holbertonschool.com>
2020-04-15 14:56:52 +0000

    fix

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,7 @@
 #ifndef SEARCH_ALGOS
 #define SEARCH_ALGOS
 #include <stdio.h>
-void print_array(int *array, size_t inc, size_t size);
+void print_array(int *array, size_t add, size_t size);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 #endif
