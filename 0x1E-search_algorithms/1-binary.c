201c2d04c1577ea37fc4d0dc131f0bd7566c7be3
rakiasomai <1069@holbertonschool.com>
2020-04-16 15:35:15 +0000

    fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,57 +1,61 @@
 #include "search_algos.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
-* binary_search - binary search a ascending sorted array
+* binary_search - binary search a ascending sorted array to find a value.
 * @array: this is a pointer
-* @size: number of elements in array
-* @value: value to search for
-* Return: -1 if error or not found
+* @size: numb of elmts
+* @value: value to search
+* Return: index or -1 in case o error
 */
 int binary_search(int *array, size_t size, int value)
 {
-int take;
-size_t a = 0;
-if (array == NULL || size == 0)
-return (-1);
-size--;
-while (a <= size)
-{
-print_array(array, a, size);
-take = (size - a) / 2 + a;
-if (array[take] == value)
-return (take);
-else if (array[take] < value)
-{
-if (size % 2 == 1)
-a = take + 1;
-else
-a = take;
+	int take;
+	size_t a = 0;
+
+	if (array == NULL || size == 0)
+		return (-1);
+	size--;
+	while (a <= size)
+	{
+		print_array(array, a, size);
+
+		take = (size - a) / 2 + a;
+		if (array[take] == value)
+			return (take);
+		else if (array[take] < value)
+		{
+			if (size % 2 == 1)
+				a = take + 1;
+			else
+				a = take;
+		}
+		else
+		{
+			if (size % 2 == 1)
+				size = take;
+			else
+				size = take - 1;
+		}
+	}
+	return (-1);
 }
-else
-{
-if (size % 2 == 1)
-size = take;
-else
-size = take - 1;
-}
-}
-return (-1);
-}
+
 /**
-* print_array - printing the current array
-* @array: this is an array
-* @add: starting incrementer index
-* @size: the size ofincremeneted idex
+* print_array - print the array
+* @array: array to print
+* @add: this is an index
+* @size: the size of inc element
 */
 void print_array(int *array, size_t add, size_t size)
 {
-printf("Searching in array: ");
-for (add = 0; add <= size; add++)
-{
-printf("%d", array[add]);
-if (add < size)
-printf(", ");
-}
-printf("\n");
+	printf("Searching in array: ");
+	for (; add <= size; add++)
+	{
+		printf("%d", array[add]);
+		if (add < size)
+			printf(", ");
+	}
+	printf("\n");
 }
